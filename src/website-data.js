57899b29647522fc43b1199697bad354c8aed69c
Bob Brady <bob@digibrady.com>
2022-07-31 15:47:53 -0400

    Reformat changes, updated AWS cert link

--- src/website-data.js
+++ src/website-data.js
@@ -5,280 +5,280 @@
  *******************************************************************************/
 
 const about = {
-  username: 'Bob Brady',
+  username: "Bob Brady",
   title: "Hi, I'm Bob Brady",
   subTitle: [
-    'I solve business problems by coding software and architecting systems',
-    'Full Stack Developer 👨‍💻 ',
-    'Cloud Solutions Architect ☁️ ',
-    'DevOps Creator 🛠',
+    "I solve business problems by coding software and architecting systems",
+    "Full Stack Developer 👨‍💻 ",
+    "Cloud Solutions Architect ☁️ ",
+    "DevOps Creator 🛠",
   ],
   resumeLink:
-    'https://drive.google.com/file/d/1MQnGVYG--G2B0tJYiERZWkNGihM_b4La/view?usp=sharing',
+    "https://drive.google.com/file/d/1MQnGVYG--G2B0tJYiERZWkNGihM_b4La/view?usp=sharing",
 };
 
 const socialMediaLinks = {
-  github: 'https://github.com/bobbrady',
-  linkedin: 'https://www.linkedin.com/in/bobbrady',
-  gmail: 'bob@digibrady.com',
+  github: "https://github.com/bobbrady",
+  linkedin: "https://www.linkedin.com/in/bobbrady",
+  gmail: "bob@digibrady.com",
 };
 
 const skills = {
   FullStackDev: {
-    title: 'Full Stack Development',
-    subtitle: 'Experience in solving problems throughout the stack',
-    image: 'UndrawProgramming',
+    title: "Full Stack Development",
+    subtitle: "Experience in solving problems throughout the stack",
+    image: "UndrawProgramming",
     skillBullets: [
-      '⚡ Deep dive legacy code refactoring to modern Java/Spring, Python/Flask, Node.js/Express backends with React/Vue/JavaScript UIs',
-      '⚡ Leverage of profiling tools, async concurrency, and computer science analysis to make performance gains up to ~10X',
-      '⚡ Architect and development of enterprise level database-backed Java/Python/Node.js REST API microservices',
-      '⚡ Implement ETL systems having cloud backends and responsive React.js frontends',
+      "⚡ Deep dive legacy code refactoring to modern Java/Spring, Python/Flask, Node.js/Express backends with React/Vue/JavaScript UIs",
+      "⚡ Leverage of profiling tools, async concurrency, and computer science analysis to make performance gains up to ~10X",
+      "⚡ Architect and development of enterprise level database-backed Java/Python/Node.js REST API microservices",
+      "⚡ Implement ETL systems having cloud backends and responsive React.js frontends",
     ],
     skillItems: [
       {
-        skillName: 'java',
-        classname: 'logos:java',
+        skillName: "java",
+        classname: "logos:java",
         style: {
-          backgroundColor: 'transparent',
+          backgroundColor: "transparent",
         },
       },
       {
-        skillName: 'python',
-        classname: 'logos:python',
+        skillName: "python",
+        classname: "logos:python",
         style: {
-          backgroundColor: 'transparent',
+          backgroundColor: "transparent",
         },
       },
       {
-        skillName: 'javascript',
-        classname: 'vscode-icons:file-type-js-official',
+        skillName: "javascript",
+        classname: "vscode-icons:file-type-js-official",
         style: {
-          backgroundColor: 'transparent',
+          backgroundColor: "transparent",
         },
       },
       {
-        skillName: 'nodeJS',
-        classname: 'vscode-icons:file-type-node',
+        skillName: "nodeJS",
+        classname: "vscode-icons:file-type-node",
         style: {
-          backgroundColor: 'transparent',
+          backgroundColor: "transparent",
         },
       },
       {
-        skillName: 'spring',
-        classname: 'logos:spring-icon',
+        skillName: "spring",
+        classname: "logos:spring-icon",
         style: {
-          backgroundColor: 'transparent',
+          backgroundColor: "transparent",
         },
       },
       {
-        skillName: 'reactJS',
-        classname: 'simple-icons:react',
+        skillName: "reactJS",
+        classname: "simple-icons:react",
         style: {
-          color: '#61DAFB',
+          color: "#61DAFB",
         },
       },
       {
-        skillName: 'flask',
-        classname: 'simple-icons:flask',
+        skillName: "flask",
+        classname: "simple-icons:flask",
         style: {
-          backgroundColor: 'transparent',
+          backgroundColor: "transparent",
         },
       },
       {
-        skillName: 'express',
-        classname: 'simple-icons:express',
+        skillName: "express",
+        classname: "simple-icons:express",
         style: {
-          backgroundColor: 'transparent',
+          backgroundColor: "transparent",
         },
       },
       {
-        skillName: 'postgres',
-        classname: 'logos:postgresql',
+        skillName: "postgres",
+        classname: "logos:postgresql",
         style: {
-          backgroundColor: 'transparent',
+          backgroundColor: "transparent",
         },
       },
       {
-        skillName: 'mysql',
-        classname: 'vscode-icons:file-type-mysql',
+        skillName: "mysql",
+        classname: "vscode-icons:file-type-mysql",
         style: {
-          backgroundColor: 'transparent',
+          backgroundColor: "transparent",
         },
       },
       {
-        skillName: 'oracle',
-        classname: 'simple-icons:oracle',
+        skillName: "oracle",
+        classname: "simple-icons:oracle",
         style: {
-          backgroundColor: 'transparent',
-          color: '#f80000',
+          backgroundColor: "transparent",
+          color: "#f80000",
         },
       },
       {
-        skillName: 'elasticsearch',
-        classname: 'logos:elasticsearch',
+        skillName: "elasticsearch",
+        classname: "logos:elasticsearch",
         style: {
-          backgroundColor: 'transparent',
+          backgroundColor: "transparent",
         },
       },
     ],
   },
   CloudSolns: {
-    title: 'Cloud Solutions',
-    subtitle: 'Leveraging cloud services with code',
-    image: 'AWS_Cloud.webp',
+    title: "Cloud Solutions",
+    subtitle: "Leveraging cloud services with code",
+    image: "AWS_Cloud.webp",
     skillBullets: [
-      '⚡ Architect and administer AWS cloud systems: virtual networks/machines, load balanced services, and auto scaling',
-      '⚡ Implement high availability database systems with multi-AZ deployments and standby instances',
-      '⚡ Code scripts for automated health check monitoring and auto-scaling actions, alert integration with Slack',
-      '⚡ Development of Azure cloud Teams chat bots with corporate LDAP directory and calendar scheduling ',
+      "⚡ Architect and administer AWS cloud systems: virtual networks/machines, load balanced services, and auto scaling",
+      "⚡ Implement high availability database systems with multi-AZ deployments and standby instances",
+      "⚡ Code scripts for automated health check monitoring and auto-scaling actions, alert integration with Slack",
+      "⚡ Development of Azure cloud Teams chat bots with corporate LDAP directory and calendar scheduling ",
     ],
     skillItems: [
       {
-        skillName: 'aws',
-        classname: 'logos:aws',
+        skillName: "aws",
+        classname: "logos:aws",
         style: {
-          backgroundColor: 'transparent',
+          backgroundColor: "transparent",
         },
       },
       {
-        skillName: 'azure',
-        classname: 'vscode-icons:file-type-azure',
+        skillName: "azure",
+        classname: "vscode-icons:file-type-azure",
         style: {
-          backgroundColor: 'transparent',
+          backgroundColor: "transparent",
         },
       },
       {
-        skillName: 'docker',
-        classname: 'logos:docker-icon',
+        skillName: "docker",
+        classname: "logos:docker-icon",
         style: {
-          backgroundColor: 'transparent',
+          backgroundColor: "transparent",
         },
       },
       {
-        skillName: 'aws vpc',
-        classname: 'logos:aws-vpc',
+        skillName: "aws vpc",
+        classname: "logos:aws-vpc",
         style: {
-          backgroundColor: 'transparent',
+          backgroundColor: "transparent",
         },
       },
       {
-        skillName: 'aws iam',
-        classname: 'logos:aws-iam',
+        skillName: "aws iam",
+        classname: "logos:aws-iam",
         style: {
-          backgroundColor: 'transparent',
+          backgroundColor: "transparent",
         },
       },
       {
-        skillName: 'aws s3',
-        classname: 'logos:aws-s3',
+        skillName: "aws s3",
+        classname: "logos:aws-s3",
         style: {
-          backgroundColor: 'transparent',
+          backgroundColor: "transparent",
         },
       },
       {
-        skillName: 'aws ec2',
-        classname: 'logos:aws-ec2',
+        skillName: "aws ec2",
+        classname: "logos:aws-ec2",
         style: {
-          backgroundColor: 'transparent',
+          backgroundColor: "transparent",
         },
       },
       {
-        skillName: 'aws rds',
-        classname: 'logos:aws-rds',
+        skillName: "aws rds",
+        classname: "logos:aws-rds",
         style: {
-          backgroundColor: 'transparent',
+          backgroundColor: "transparent",
         },
       },
       {
-        skillName: 'aws fargate',
-        classname: 'logos:aws-fargate',
+        skillName: "aws fargate",
+        classname: "logos:aws-fargate",
         style: {
-          backgroundColor: 'transparent',
+          backgroundColor: "transparent",
         },
       },
       {
-        skillName: 'linux',
-        classname: 'logos:linux-tux',
+        skillName: "linux",
+        classname: "logos:linux-tux",
         style: {
-          backgroundColor: 'transparent',
+          backgroundColor: "transparent",
         },
       },
       {
-        skillName: 'bash',
-        classname: 'logos:bash-icon',
+        skillName: "bash",
+        classname: "logos:bash-icon",
         style: {
-          backgroundColor: 'transparent',
+          backgroundColor: "transparent",
         },
       },
     ],
   },
   DevOps: {
-    title: 'DevOps',
-    subtitle: 'Building, Scanning, and Shipping Code with Pipelines',
-    image: 'devops.svg',
+    title: "DevOps",
+    subtitle: "Building, Scanning, and Shipping Code with Pipelines",
+    image: "devops.svg",
     skillBullets: [
-      '⚡ Hands-on lead of GitLab CI/CD pipeline migration from Gitea/Jenkins for 20+ repos',
-      '⚡ Develop Maven build, test, Sonar quality, Whitesource security scan, and Artifactory deployment pipeline templates',
-      '⚡ Implement Python automation scripts for load test benchmarking, Git admin, and health checks',
-      '⚡ Code and debug legacy Jenkins pipelines',
+      "⚡ Hands-on lead of GitLab CI/CD pipeline migration from Gitea/Jenkins for 20+ repos",
+      "⚡ Develop Maven build, test, Sonar quality, Whitesource security scan, and Artifactory deployment pipeline templates",
+      "⚡ Implement Python automation scripts for load test benchmarking, Git admin, and health checks",
+      "⚡ Code and debug legacy Jenkins pipelines",
     ],
     skillItems: [
       {
-        skillName: 'git',
-        classname: 'logos:github-octocat',
+        skillName: "git",
+        classname: "logos:github-octocat",
         style: {
-          backgroundColor: 'transparent',
+          backgroundColor: "transparent",
         },
       },
       {
-        skillName: 'gitlab',
-        classname: 'logos:gitlab',
+        skillName: "gitlab",
+        classname: "logos:gitlab",
         style: {
-          backgroundColor: 'transparent',
+          backgroundColor: "transparent",
         },
       },
       {
-        skillName: 'jenkins',
-        classname: 'vscode-icons:file-type-jenkins',
+        skillName: "jenkins",
+        classname: "vscode-icons:file-type-jenkins",
         style: {
-          backgroundColor: 'transparent',
+          backgroundColor: "transparent",
         },
       },
       {
-        skillName: 'maven',
-        classname: 'vscode-icons:file-type-maven',
+        skillName: "maven",
+        classname: "vscode-icons:file-type-maven",
         style: {
-          backgroundColor: 'transparent',
+          backgroundColor: "transparent",
         },
       },
       {
-        skillName: 'docker',
-        classname: 'logos:docker-icon',
+        skillName: "docker",
+        classname: "logos:docker-icon",
         style: {
-          backgroundColor: 'transparent',
+          backgroundColor: "transparent",
         },
       },
       {
-        skillName: 'python',
-        classname: 'logos:python',
+        skillName: "python",
+        classname: "logos:python",
         style: {
-          backgroundColor: 'transparent',
+          backgroundColor: "transparent",
         },
       },
       {
-        skillName: 'splunk',
-        classname: 'simple-icons:splunk',
+        skillName: "splunk",
+        classname: "simple-icons:splunk",
         style: {
-          backgroundColor: 'transparent',
-          color: 'blue',
+          backgroundColor: "transparent",
+          color: "blue",
         },
       },
       {
-        skillName: 'sonarqube',
-        classname: 'simple-icons:sonarqube',
+        skillName: "sonarqube",
+        classname: "simple-icons:sonarqube",
         style: {
-          backgroundColor: 'transparent',
-          color: 'blue',
+          backgroundColor: "transparent",
+          color: "blue",
         },
       },
     ],
@@ -290,57 +290,57 @@ const skills = {
 const certifications = {
   certifications: [
     {
-      title: 'Amazon',
-      subtitle: 'AWS Solutions Architect Associate',
-      logo_path: 'aws-certified-solutions-architect-associate.png',
+      title: "Amazon",
+      subtitle: "AWS Solutions Architect Associate",
+      logo_path: "aws-certified-solutions-architect-associate.png",
       certificate_link:
-        'https://www.credly.com/earner/earned/badge/f5ed8ac1-7480-4943-a889-ab97fde6ec33',
-      alt_name: 'Amazon AWS Solutions Architect Associate',
-      color_code: '#F6EAEA',
+        "https://www.credly.com/badges/f5ed8ac1-7480-4943-a889-ab97fde6ec33",
+      alt_name: "Amazon AWS Solutions Architect Associate",
+      color_code: "#F6EAEA",
     },
     {
-      title: 'Google',
-      subtitle: 'Google IT Automation with Python',
-      logo_path: 'google-it-automation-certificate.png',
+      title: "Google",
+      subtitle: "Google IT Automation with Python",
+      logo_path: "google-it-automation-certificate.png",
       certificate_link:
-        'https://www.credly.com/badges/eb7f3b8b-8311-486b-b542-424ecab69a28/public_url',
-      alt_name: 'Google IT Automation with Python',
-      color_code: '#F0FFDD',
+        "https://www.credly.com/badges/eb7f3b8b-8311-486b-b542-424ecab69a28/public_url",
+      alt_name: "Google IT Automation with Python",
+      color_code: "#F0FFDD",
     },
     {
-      title: 'Oracle',
-      subtitle: 'Oracle Java Professional Programmer',
-      logo_path: 'oracle-certified-professional-java-se-7-programmer.png',
+      title: "Oracle",
+      subtitle: "Oracle Java Professional Programmer",
+      logo_path: "oracle-certified-professional-java-se-7-programmer.png",
       certificate_link:
-        'https://www.credly.com/badges/32018933-b6d7-43ee-bc2e-138b4d76f410/public_url',
-      alt_name: 'Oracle Java Professional Programmer',
-      color_code: '#FEF6E6',
+        "https://www.credly.com/badges/32018933-b6d7-43ee-bc2e-138b4d76f410/public_url",
+      alt_name: "Oracle Java Professional Programmer",
+      color_code: "#FEF6E6",
     },
     {
-      title: 'RedHat',
-      subtitle: 'RedHat Certified Engineer',
-      logo_path: 'red-hat-certified-engineer.jpeg',
-      certificate_link: 'https://rhtapps.redhat.com/verify?certId=140-104-100',
-      alt_name: 'RedHat Certified Engineer',
-      color_code: '#E3F1FA',
+      title: "RedHat",
+      subtitle: "RedHat Certified Engineer",
+      logo_path: "red-hat-certified-engineer.jpeg",
+      certificate_link: "https://rhtapps.redhat.com/verify?certId=140-104-100",
+      alt_name: "RedHat Certified Engineer",
+      color_code: "#E3F1FA",
     },
     {
-      title: 'CompTIA',
-      subtitle: 'CompTIA Linux+',
-      logo_path: 'comptia-linux-powered-by-lpi-certification.png',
+      title: "CompTIA",
+      subtitle: "CompTIA Linux+",
+      logo_path: "comptia-linux-powered-by-lpi-certification.png",
       certificate_link:
-        'https://www.credly.com/badges/41ea3150-208a-40af-8c23-ed5125e8f6a2/public_url',
-      alt_name: 'CompTIA Linux+',
-      color_code: '#FDE9EA',
+        "https://www.credly.com/badges/41ea3150-208a-40af-8c23-ed5125e8f6a2/public_url",
+      alt_name: "CompTIA Linux+",
+      color_code: "#FDE9EA",
     },
   ],
 };
 
 const contactInfo = {
-  title: 'Contact Me',
+  title: "Contact Me",
   subtitle:
-    'Discuss a project or just want to say hi? My Inbox is open for all.',
-  email_address: 'bob@digibrady.com',
+    "Discuss a project or just want to say hi? My Inbox is open for all.",
+  email_address: "bob@digibrady.com",
 };
 
 export { about, socialMediaLinks, skills, contactInfo, certifications };
